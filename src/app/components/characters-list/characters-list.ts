938817e85e60b6011acc12ee79a54aa8163ce67b
Nicolas Bernard <njr.bernard@gmail.com>
2025-12-10 12:03:08 +0100

    initial commit

---
+++ src/app/components/characters-list/characters-list.ts
@@ -0,0 +1,40 @@
+import { Component } from '@angular/core';
+import { Characters } from '../../shared/models/characters.model';
+
+@Component({
+  selector: 'app-characters-list',
+  imports: [],
+  templateUrl: './characters-list.html',
+  styleUrl: './characters-list.scss',
+})
+export class CharactersList {
+protected newCharacter: Characters = {
+    id: 1,
+    firstName: 'Nicolas',
+    lastName: 'Bernard',
+    title: 'GOAT',
+    image: '',
+    imageUrl: ''
+}
+
+protected charactersFromApi: Characters[] = [
+  {
+    id: 2,
+    firstName: 'Martin',
+    lastName: 'Ferret',
+    family: 'Pieds carrés',
+    title: 'Roi',
+    image: '',
+    imageUrl: ''
+  },
+  {
+    id: 3,
+    firstName: 'Xavier',
+    lastName: 'Esta',
+    family: 'Linux',
+    title: 'Seigneur',
+    image: '',
+    imageUrl: ''
+  }
+];
+}
