346fd90fce1dc72a3626810807f0d179c6e395f1
Nicolas Bernard <njr.bernard@gmail.com>
2025-12-16 14:59:25 +0100

    MAJ avant déploiement

--- src/app/shared/services/continents.ts
+++ src/app/shared/services/continents.ts
@@ -2,15 +2,16 @@ import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { Observable } from 'rxjs/internal/Observable';
 import { Continents } from '../models/continents-model';
+import { environment } from '../../../environments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ContinentService {
-  private continentsUrl: string = 'https://thronesapi.com/api/v2/Continents';
+  private continentsUrl: string = environment.apiUrl;
   private httpClient = inject(HttpClient);
 
   public getAllContinents() : Observable<Continents[]> {
-    return this.httpClient.get<Continents[]>(this.continentsUrl);
+    return this.httpClient.get<Continents[]>(this.continentsUrl + '/Continents');
   }
 }
